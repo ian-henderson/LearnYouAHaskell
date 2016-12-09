0b2193fa44c4ae73ab06b2054fa3c7fe8819752f
Ian Henderson <iancurtish@gmail.com>
2016-12-09 10:26:59 -0800

    first commit

---
+++ baby.hs
@@ -0,0 +1,11 @@
+doubleMe x = x*2
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+                        then x
+                        else doubleMe x
+
+doubleSmallNumber' x = (if x > 100 then x else doubleMe x) + 1
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
