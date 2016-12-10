8eba3e6c73bb0e9343e7e4a50a25446049590065
Ian Henderson <iancurtish@gmail.com>
2016-12-10 11:02:04 -0800

    starting out work

--- baby.hs
+++ baby.hs
@@ -2,10 +2,12 @@ doubleMe x = x*2
 
 doubleUs x y = doubleMe x + doubleMe y
 
-doubleSmallNumber x = if x > 100
-                        then x
-                        else doubleMe x
+doubleSmallNumber x = if x > 100 then x else doubleMe x
 
 doubleSmallNumber' x = (if x > 100 then x else doubleMe x) + 1
 
 conanO'Brien = "It's a-me, Conan O'Brien!"
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+
+length' xs = sum [1 | _ <- xs]
